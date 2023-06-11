3b2f95d70adc1cae3f47e912c75fce946a079d53
rif006 <rif.006@gmail.com>
2023-06-12 02:32:05 +0800

    Membuat Class Matematika Beraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -3,5 +3,12 @@ package hitung;
 
 
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        Matematika arif = new Matematika(6,1);
+        
+        System.out.println("Hasil Penjumlahan = " + arif.setPenjumlahan());
+        System.out.println("Hasil Pengurangan = " + arif.setPengurangan());
+        System.out.println("Hasil Perkalian = " + arif.setPerkalian());
+        System.out.println("Hasil Pembagian = " + arif.setPembagian());
+    }
 }
